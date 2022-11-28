a7f3c64ec4f9e61c40a5a8344b7cd0a348630b9c
georgikolev17 <g.t.kolev1@gmail.com>
2022-11-28 19:53:07 +0200

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using CrashVisualizer.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.IO;
+
+namespace CrashVisualizer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string stateName)
+        {
+            StateViewModel model = new StateViewModel(stateName, 19840000, 141300, 10000);
+            return View("StateInfo", model);
+        }
+
+        public async Task<IActionResult> Visualise(string id)
+        {
+            if (id == "New York")
+            {
+                return this.File(await System.IO.File.ReadAllBytesAsync("./wwwroot/local.html"), "text/html");
+            }
+            return this.View("Index");
+        }
+
+        public async Task<IActionResult> Prediction(string id)
+        {
+            if (id == "New York")
+            {
+                return this.File(await System.IO.File.ReadAllBytesAsync("./wwwroot/local1.html"), "text/html");
+            }
+            return this.View("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/SearchModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrashVisualizer.Models
+{
+    public class SearchModel
+    {
+        [Display(Name = "Select USA state")]
+        public UsaStateName UsaStateName { get; set; }
+    }
+}
