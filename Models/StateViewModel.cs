f4d213cc40fb032fc10f70fb4773ed520ff01a59
georgikolev17 <g.t.kolev1@gmail.com>
2022-11-28 19:53:07 +0200

    Initial commit

---
+++ Models/StateViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace CrashVisualizer.Models
+{
+    public class StateViewModel
+    {
+        public StateViewModel(string name, int population, int area, int crashesPerYear)
+        {
+            Name=name;
+            Population=population;
+            Area=area;
+            CrashesPerYear=crashesPerYear;
+        }
+
+        public string Name { get; set; }
+
+        public int Population { get; set; }
+
+        public int Area { get; set; }
+
+        public int CrashesPerYear { get; set; }
+    }
+}
